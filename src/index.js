28f02ab4f2ef62f10928bdd11c1d4de9942dedf4
AbnerAWR <AbnerW_@hotmail.com>
2021-02-12 20:09:27 -0300

    src update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './components/Root';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Root />,
+    document.getElementById('root')
+
 );
