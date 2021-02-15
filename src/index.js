aec8f285e9234a49f18ffa2b518b137c1227b0a9
AbnerAWR <AbnerW_@hotmail.com>
2021-02-15 19:33:24 -0300

    src update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './';
 import Root from './components/Root';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 ReactDOM.render(
+    
     <Root />,
     document.getElementById('root')
 
