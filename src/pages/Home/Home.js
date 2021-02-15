472cc3ea7ccecc0f8b9f8f9e64699320218310ba
AbnerAWR <AbnerW_@hotmail.com>
2021-02-15 19:33:07 -0300

    home update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,8 @@
-import { Row, Col, Container } from 'react-bootstrap'
+import { Row, Col, Container, Button } from 'react-bootstrap'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 
-import './Home.css'
+import './Home.scss'
 
 function Home() {
     return (
@@ -11,26 +11,39 @@ function Home() {
         <Container fluid>
             
             <Row>
-                <Col sm={8}>
-                    <h1>Home</h1>
-                </Col>
-
-                <Col sm={4}>
+                <Col sm={8} className="bg-dark">
+                    <h1 className="teste">Home</h1>
+                    
+                </Col>                
+                <Col sm={4} className="bg-primary coluna-esquerda">
+                    <img src="https://i.ibb.co/Jj1Yqrt/62721361.jpg" alt="photo" className="img-photo"></img>
                     <h2>
                         <span className="span-name">
                             Abner Wesley Ribeiro
                         </span>
                     </h2>
-
                     <div>
                         <p>
                             <span>
-                                <a href="mailto:AbnerW_@hotmail.com">AbnerW_@hotmail.com</a>
+                                <a href="mailto:AbnerW_@hotmail.com" className="email">AbnerW_@hotmail.com</a>
                             </span>
                         </p>
                         <p>
-                            <span>(19)99912-5210 </span>
+                            <span className="span-number">(19)99912-5210 </span>
                         </p>
+                        <a href="https://www.linkedin.com/in/abner-wesley-ribeiro-6731bb171/">
+                        <img src="https://static.wixstatic.com/media/7528824071724d12a3e6c31eee0b40d4.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/7528824071724d12a3e6c31eee0b40d4.webp"></img>
+                        </a>
+                        <a href="">
+                        <img src="https://static.wixstatic.com/media/23fd2a2be53141ed810f4d3dcdcd01fa.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/23fd2a2be53141ed810f4d3dcdcd01fa.webp"></img>
+                        </a>
+                        <a href="">
+                        <img src="https://static.wixstatic.com/media/01ab6619093f45388d66736ec22e5885.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/01ab6619093f45388d66736ec22e5885.webp"></img>
+                        </a>
+                        <a href="">
+                        <img src="https://static.wixstatic.com/media/81af6121f84c41a5b4391d7d37fce12a.png/v1/fill/w_24,h_24,al_c,q_85,usm_0.66_1.00_0.01/81af6121f84c41a5b4391d7d37fce12a.webp"></img>
+                        </a>
+                        
                     </div>
                 </Col>
             </Row>
