5a68f22c3aa635b7dc23e352e1b4ac59423f610f
AbnerAWR <AbnerW_@hotmail.com>
2021-03-03 16:18:46 -0300

    routes update

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import About from '../pages/About/About'
-import Contact from '../pages/Contact/Contact'
+// import About from '../pages/About/About'
+// import Contact from '../pages/Contact/Contact'
 import Home from '../pages/Home/Home'
 
 function Routes(){
@@ -10,9 +10,9 @@ function Routes(){
         <BrowserRouter>
 
             <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
+            {/* <Route exact path="/about" component={About} />
             <Route exact path="/contact" component={Contact} />
-         
+          */}
         </BrowserRouter>
     )
 }
