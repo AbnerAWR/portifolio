3e2865448e932c5218f1aca3ffa5be9b7a765282
AbnerAWR <AbnerW_@hotmail.com>
2021-03-03 16:18:32 -0300

    footer update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-import {ReactComponent as IconsLinkedin } from '../../assets/Linkedin.svg'
-
 import './Footer.css'
 
 function  Footer() {
     return (
         <div>
             <footer className="main-footer">
-                {/* <div className="footer-icons">
-                    <a href="https://www.linkedin.com/in/abner-wesley-ribeiro-6731bb171/">
-                        <IconsLinkedin />
-                    </a>
-                </div> */}
+               <div>
+                   <p> Desenvolvido por Abner Wesley Ribeiro.</p>
+               </div>
             </footer>
         </div>
     )
